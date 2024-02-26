694a67bd954197f72b8a97e9e3664ef29a51c3ab
CBowne <156275809+CBowneUCF@users.noreply.github.com>
2024-02-25 20:14:18 -0500

    Monster SightCheck Improvement

--- Assets/Scripts/StatueEnemyScript.cs
+++ Assets/Scripts/StatueEnemyScript.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Rendering.Universal;
+using Vector3Helper;
 
 public class StatueEnemyScript : MonoBehaviour
 {
@@ -10,50 +13,59 @@ public class StatueEnemyScript : MonoBehaviour
     public float moveSpeed;
     public float attackRadius;
     public LayerMask sightLayerMask;
-    public float statueRadius;
-
-    //public NavMeshObstacle frustrumObstacle;
+    public float sightPadding;
+    public float killRadius;
     
     //Components
     NavMeshAgent agent;
     new Transform transform;
-    new Renderer renderer;
+    private new Renderer renderer;
+    Transform rendererTransform;
+
     public Transform player;
     public Camera playerCamera;
     public Transform sneakUpTarget;
+    public BoxCollider renderBounds;
+
 
     //Other Data
     //bool playerLookingInDirection;
     bool canMove;
     //Ray rayToPlayer;
-    
+    private LayerMask sightLayerMask_Old;
+    private float statueRadius_Old;
+    //Plane[] viewPlanes;
+    //[SerializeField] float dDistance;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         transform = base.transform;
-        renderer = GetComponent<Renderer>();
+        renderer = GetComponentInChildren<Renderer>();
+        rendererTransform = renderBounds.transform;
     }
 
 
     void Update()
     {
-        //rayToPlayer = new Ray(transform.position, (player.position - transform.position).normalized);
-        //playerLookingInDirection = Vector3.Dot(rayToPlayer.direction, player.eulerAngles) <= viewRadius;
-        //frustrumObstacle.enabled = (Vector3.Dot(rayToPlayer.direction, player.eulerAngles) >= -0.9);
-
+        //viewPlanes = GeometryUtility.CalculateFrustumPlanes(playerCamera);
 
         DetermineMovementCapability();
 
-        SetSneakTargetPosition();
+        //SetSneakTargetPosition();
         
         if (!agent.isStopped)
         {
+            agent.destination = player.position;
+            /*
             if (Vector3.Distance(player.position, transform.position) <= attackRadius) agent.destination = player.position;
             else agent.destination = sneakUpTarget.position;
+             */
         }
 
-
+        //dDistance = Vector3.Distance(transform.position, player.position);
+        Debug.DrawRay(transform.position, (player.position-transform.position).normalized * killRadius, Color.red);
+        if (canMove && (Vector3.Distance(transform.position, player.position) <= killRadius)) Debug.Log("SNAP");
 
     }
 
@@ -62,14 +74,21 @@ public class StatueEnemyScript : MonoBehaviour
         if (!IsPlayerLooking()) ChangeMovementCapability(true);
         else
         {
-            if (IsSightBlocked()) ChangeMovementCapability(true);
+            if (IsSightBlockedV3()) ChangeMovementCapability(true);
             else ChangeMovementCapability(false);
         }
+        
+
     }
     void ChangeMovementCapability(bool value)
     {
+        canMove = value;
         agent.isStopped = !value;
-        if(!value) agent.velocity = Vector3.zero;
+        if (!value)
+        {
+            agent.velocity = Vector3.zero;
+            agent.destination = agent.nextPosition;
+        }
     }
     void SetSneakTargetPosition()
     {
@@ -92,25 +111,25 @@ public class StatueEnemyScript : MonoBehaviour
 
     //Frustrum Check Code from https://youtu.be/_e57zSZSOS8
     bool IsPlayerLooking() => GeometryUtility.TestPlanesAABB(GeometryUtility.CalculateFrustumPlanes(playerCamera), renderer.bounds);
-    bool IsSightBlocked()
+    bool IsSightBlockedV1()
     {
         float midDistance = Vector3.Distance(transform.position, player.position);
         Vector3 midDirection = (transform.position - player.position).normalized;
         RaycastHit hit;
-        Physics.Raycast(player.position, midDirection, out hit, midDistance, sightLayerMask);
+        Physics.Raycast(player.position, midDirection, out hit, midDistance, sightLayerMask_Old);
 
         bool midHit = (hit.transform == transform);
 
-        float angle = Mathf.Rad2Deg*Mathf.Atan(statueRadius/midDistance);
+        float angle = Mathf.Rad2Deg*Mathf.Atan(statueRadius_Old/midDistance);
 
         Vector3 leftDirection = Quaternion.AngleAxis(-angle, Vector3.up) * midDirection;
         Vector3 rightDirection = Quaternion.AngleAxis(angle, Vector3.up) * midDirection; ;
 
-        float sideDistance = Mathf.Sqrt(Mathf.Pow(midDistance, 2) + Mathf.Pow(statueRadius, 2));
+        float sideDistance = Mathf.Sqrt(Mathf.Pow(midDistance, 2) + Mathf.Pow(statueRadius_Old, 2));
 
-        Physics.Raycast(player.position, leftDirection, out hit, sideDistance, sightLayerMask);
+        Physics.Raycast(player.position, leftDirection, out hit, sideDistance, sightLayerMask_Old);
         bool leftHit = (hit.transform == transform);
-        Physics.Raycast(player.position, rightDirection, out hit, sideDistance, sightLayerMask);
+        Physics.Raycast(player.position, rightDirection, out hit, sideDistance, sightLayerMask_Old);
         bool rightHit = (hit.transform == transform);
 
         Debug.DrawRay(player.position, midDirection * midDistance, midHit? Color.white:Color.red);
@@ -120,6 +139,57 @@ public class StatueEnemyScript : MonoBehaviour
 
         return !(midHit || rightHit || leftHit);
     }
+    
+    bool IsSightBlockedV2()
+    {
+        Position playerPos = player.transform.position;
+        Position enemyPosCenter = transform.position;
+
+        Direction enemyToPlayerDirection = new Direction(playerPos - enemyPosCenter).normalized;
+        Direction enemyToPlayerPerpendicular = enemyToPlayerDirection.Rotate(90, Direction.up);
+
+        bool midHit = SeeRay(enemyPosCenter);
+        bool leftHit = SeeRay(enemyPosCenter - (enemyToPlayerPerpendicular * statueRadius_Old));
+        bool rightHit = SeeRay(enemyPosCenter + (enemyToPlayerPerpendicular * statueRadius_Old));
+
+        return !(midHit || rightHit || leftHit);
+    }
+
+    bool IsSightBlockedV3()
+    {
+        //Bounds rendererBounds = renderer.bounds;
+        Position centerPos = rendererTransform.TransformPoint(renderBounds.center);
+
+        Position forwardRightPos = (Vector3)centerPos + (rendererTransform.forward * (renderBounds.size.z/2 + sightPadding))    + (rendererTransform.right * (renderBounds.size.x/2 + sightPadding));
+        Position forwardLeftPos =  (Vector3)centerPos + (rendererTransform.forward * (renderBounds.size.z/2 + sightPadding))    - (rendererTransform.right * (renderBounds.size.x/2 + sightPadding));
+        Position backRightPos =    (Vector3)centerPos - (rendererTransform.forward * (renderBounds.size.z/2 + sightPadding))    + (rendererTransform.right * (renderBounds.size.x/2 + sightPadding));
+        Position backLeftPos =     (Vector3)centerPos - (rendererTransform.forward * (renderBounds.size.z / 2 + sightPadding))  - (rendererTransform.right * (renderBounds.size.x/2 + sightPadding));
+
+
+        bool originHit = SeeRay(transform.position);
+        bool midHit = SeeRay(centerPos);
+        bool forwardRightHit = SeeRay(forwardRightPos);
+        bool backRightHit = SeeRay(backRightPos);
+        bool forwardLeftHit = SeeRay(forwardLeftPos);
+        bool backLeftHit = SeeRay(backLeftPos);
+
+
+        return !(originHit || midHit || forwardLeftHit || forwardRightHit || backLeftHit || backRightHit);
+    }
+
+    bool SeeRay(Position start)
+    {
+        Vector3 withinCam = playerCamera.WorldToViewportPoint(start);
+        if (!(withinCam.z > 0 && withinCam.x < 1 && withinCam.x > 0 && withinCam.y < 1 && withinCam.y > 0)) return false;
+
+        RaycastHit hit;
+        Physics.Linecast(start, player.transform.position, out hit, sightLayerMask);
+        bool result = hit.transform == player.transform;
+
+        Debug.DrawLine(start, player.transform.position, result ? Color.white : Color.red);
+        return result;
+    }
+
 
 
 
